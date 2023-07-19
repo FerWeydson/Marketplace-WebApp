6e74c9f29b2d88d879549cf93bf13e14d20d277f
Weydson Fernandes <Weydson Fernandes@DESKTOP-HFRMBH8>
2023-07-19 10:57:09 -0300

    Downgrade na versão dos pacotes para gerar o QRCode. Renderizando o QRCode normalmente.

--- Web_ECommerce/Models/HelpQrCode.cs
+++ Web_ECommerce/Models/HelpQrCode.cs
@@ -30,9 +30,10 @@ namespace Web_ECommerce.Models
 
         private static byte[] BitmapToBytes(Bitmap img) 
         {
-            using(MemoryStream stream = new MemoryStream())
+            using (MemoryStream stream = new MemoryStream())
             {
                 img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+
                 return stream.ToArray();
             }
         }
@@ -41,8 +42,9 @@ namespace Web_ECommerce.Models
         {
             using (var doc = new PdfSharpCore.Pdf.PdfDocument())
             {
-                #region Configurações da folha
+                #region Configuracoes da folha
                 var page = doc.AddPage();
+
                 page.Size = PdfSharpCore.PageSize.A4;
                 page.Orientation = PdfSharpCore.PageOrientation.Portrait;
 
@@ -50,66 +52,81 @@ namespace Web_ECommerce.Models
                 var corFonte = XBrushes.Black;
                 #endregion
 
-                #region Numeração das páginas
+                #region Numeração das Páginas
                 int qtdPaginas = doc.PageCount;
+
                 var numeracaoPagina = new PdfSharpCore.Drawing.Layout.XTextFormatter(graphics);
                 numeracaoPagina.DrawString(Convert.ToString(qtdPaginas), new PdfSharpCore.Drawing.XFont("Arial", 10), corFonte, new PdfSharpCore.Drawing.XRect(575, 825, page.Width, page.Height));
                 #endregion
 
-                #region Logo
+                #region Logo 
                 var webRoot = _environment.WebRootPath;
 
                 var logoFatura = string.Concat(webRoot, "/img/", "loja-virtual-1.png");
+
                 XImage imagem = XImage.FromFile(logoFatura);
+
                 graphics.DrawImage(imagem, 20, 5, 300, 50);
                 #endregion
 
                 #region Informações 1
                 var relatorioCobranca = new PdfSharpCore.Drawing.Layout.XTextFormatter(graphics);
+
                 var titulo = new PdfSharpCore.Drawing.XFont("Arial", 14, PdfSharpCore.Drawing.XFontStyle.Bold);
+
                 relatorioCobranca.Alignment = PdfSharpCore.Drawing.Layout.XParagraphAlignment.Center;
+
                 relatorioCobranca.DrawString("BOLETO ONLINE", titulo, corFonte, new XRect(0, 65, page.Width, page.Height));
+
                 #endregion
 
+
                 #region Informações 2
+
                 var alturaTituloDetalhesY = 120;
+
                 var detalhes = new PdfSharpCore.Drawing.Layout.XTextFormatter(graphics);
-                var tituloInfo_1 = new PdfSharpCore.Drawing.XFont("Arial", 18, XFontStyle.Regular);
+
+                var tituloInfo_1 = new PdfSharpCore.Drawing.XFont("Arial", 8, XFontStyle.Regular);
 
                 detalhes.DrawString("Dados do banco", tituloInfo_1, corFonte, new XRect(25, alturaTituloDetalhesY, page.Width, page.Height));
                 detalhes.DrawString("Banco Itau 004", tituloInfo_1, corFonte, new XRect(150, alturaTituloDetalhesY, page.Width, page.Height));
 
-                alturaTituloDetalhesY += 20;
-                detalhes.DrawString("Código gerado", tituloInfo_1, corFonte, new XRect(25, alturaTituloDetalhesY, page.Width, page.Height));
+                alturaTituloDetalhesY += 9;
+                detalhes.DrawString("Código Gerado", tituloInfo_1, corFonte, new XRect(25, alturaTituloDetalhesY, page.Width, page.Height));
                 detalhes.DrawString("000000 000000 000000 000000", tituloInfo_1, corFonte, new XRect(150, alturaTituloDetalhesY, page.Width, page.Height));
 
-                alturaTituloDetalhesY += 20;
+
+                alturaTituloDetalhesY += 9;
                 detalhes.DrawString("Quantidade:", tituloInfo_1, corFonte, new XRect(25, alturaTituloDetalhesY, page.Width, page.Height));
                 detalhes.DrawString(compraUsuario.QuantidadeProduto.ToString(), tituloInfo_1, corFonte, new XRect(150, alturaTituloDetalhesY, page.Width, page.Height));
 
-                alturaTituloDetalhesY += 20;
-                detalhes.DrawString("Valor total:", tituloInfo_1, corFonte, new XRect(25, alturaTituloDetalhesY, page.Width, page.Height));
-                detalhes.DrawString(compraUsuario.ValorTotal.ToString(), tituloInfo_1, corFonte, new XRect(150, alturaTituloDetalhesY, page.Width, page.Height));
-                
+                alturaTituloDetalhesY += 9;
+                detalhes.DrawString("Valor Total:", tituloInfo_1, corFonte, new XRect(25, alturaTituloDetalhesY, page.Width, page.Height));
+                detalhes.DrawString(compraUsuario.QuantidadeProduto.ToString(), tituloInfo_1, corFonte, new XRect(150, alturaTituloDetalhesY, page.Width, page.Height));
+
                 var tituloInfo_2 = new PdfSharpCore.Drawing.XFont("Arial", 8, XFontStyle.Bold);
+
 
                 try
                 {
                     var img = await GerarQrCode("Dados do banco aqui");
+
                     Stream streamImage = new MemoryStream(img);
 
-                    XImage qrCode = XImage.FromStream(()  => streamImage);
+                    XImage qrCode = XImage.FromStream(() => streamImage);
 
                     alturaTituloDetalhesY += 40;
-                    graphics.DrawImage(qrCode, 200, alturaTituloDetalhesY, 150, 150);
+                    graphics.DrawImage(qrCode, 140, alturaTituloDetalhesY, 310, 310);
                 }
-                catch (Exception)
+                catch (Exception erro)
                 {
-                    
+
                 }
 
                 alturaTituloDetalhesY += 620;
-                detalhes.DrawString("Canhoto para pagamento online", tituloInfo_2, corFonte, new XRect(20, alturaTituloDetalhesY, page.Width, page.Height));
+                detalhes.DrawString("Canhoto com QrCode para pagamento online.", tituloInfo_2, corFonte, new XRect(20, alturaTituloDetalhesY, page.Width, page.Height));
+
                 #endregion
 
                 using (MemoryStream stream = new MemoryStream())
@@ -117,7 +134,7 @@ namespace Web_ECommerce.Models
                     var contentType = "application/pdf";
 
                     doc.Save(stream, false);
-                    return File(stream.ToArray(), contentType, "Boleto.pdf");
+                    return File(stream.ToArray(), contentType, "BoletoLojaOnline.pdf");
                 }
 
             }
